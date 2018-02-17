b261f87e21a94ce97accc0583f5b4a2e9ec70f2f
David Chen <chen749@usc.edu>
2018-02-17 12:48:55 -0800

    finished example erc721

--- contracts/ExampleERC721.sol
+++ contracts/ExampleERC721.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.18;
 
 import "zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
 
@@ -9,7 +9,8 @@ contract exampleERC721 is ERC721Token{
 	address private creator;
 	uint256 constant private price = 50 ether;
 	string constant private creatorName = "Billy Lau";
-	string constant private creatorBio = "I am a third year student at the University of British Columbia. I aspire to be a future blockchain entrepreneur.";
+	string constant private creatorBio = "I am a third year student at the University of British Columbia. I aspire to be a future blockchain entrepreneur. Currently, I have student debt of amount $5000. I wish to raise funds so I can continue my education studying blockchain technology at Stanford.";
+	bool private donateLimitReached = false;
 
 	event Donated(address donor);
 	
@@ -33,10 +34,12 @@ contract exampleERC721 is ERC721Token{
 	
 	//Mint
 	function mint () public payable{
-		require(msg.value >= price);
-		_mint(msg.sender,0);
-		creator.transfer(msg.value);
-		Donated(msg.sender);
+		require(!donateLimitReached); //check if someone has already donated
+		require(msg.value >= price); //check if donation amount is greater or equal to requested amount
+		_mint(msg.sender,0); //create the 0th token and send it to the donor
+		creator.transfer(msg.value); //transfer the donation ETH amount to the creator
+		Donated(msg.sender); //fire an event signalling donation has been sucessful
+		donateLimitReached = true; //flag to prevent multiple donations to the same token
 	}
 	
 
